eac7a617d821a4d1d18ee0b14a8f4df8bbfc37a5
john <john@circlesabound.com>
2016-10-01 16:57:18 +1000

    database build script

--- server/db.py
+++ server/db.py
@@ -18,7 +18,12 @@ class db(object):
 	def check_credentials(self, email, password):
 		hashed = hashlib.sha256(password)
 		c = self.conn.cursor()
-		c.execute('SELECT Count(*) FROM users WHERE email=? AND password=?', email, hashed.hexdigest())
-		(number_of_rows, ) = c.fetchone()
-		return number_of_rows == 1
-		
+		c.execute('SELECT Count(*), id FROM users WHERE email=? AND password=?', email, hashed.hexdigest())
+		(number_of_rows, user_id) = c.fetchone()
+		if number_of_rows is not None:
+			return (int)user_id
+		else:
+			return None
+
+	def get_next_match(self, id, ):
+		pass
--- server/db_rebuild.py
+++ server/db_rebuild.py
@@ -1,4 +1,12 @@
+#!/usr/bin/python3
+
 import sqlite3
+import os.path
+import os
+
+if os.path.isfile('matchr.db'):
+	print("removing old db")
+	os.remove('matchr.db')
 
 conn = sqlite3.connect('matchr.db')
 
@@ -9,23 +17,23 @@ conn.execute('''CREATE TABLE users (id INTEGER PRIMARY KEY, fName TEXT, lName TE
 conn.execute('''CREATE TABLE relationship (relationshipScore INT, idFirst INT, 
 	idSecond INT, statusFirst INT, statusSecond INT);''')
 
-currentSession = null
-conn.execute('''SELECT idSecond FROM relationship WHERE statusFirst = NULL AND idFirst = ? ORDER BY relationshipScore;''', currentSession)
-conn.execute('''SELECT idFirst FROM relationship WHERE statusSecond = NULL AND idSecond = ? ORDER BY relationshipScore;''', currentSession)
+currentSession = (None,)
+conn.execute('''SELECT idSecond FROM relationship WHERE statusFirst = NULL AND idFirst = ? ORDER BY relationshipScore DESC;''', currentSession)
+conn.execute('''SELECT idFirst FROM relationship WHERE statusSecond = NULL AND idSecond = ? ORDER BY relationshipScore DESC;''', currentSession)
 
+"""
 conn.execute('''INSERT INTO users (fName, lName, gender, image, description, email, password, bracePlacement, spaceOrTab, indentAmount, 
 	varConvention, commentStyle, maxLineLength) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);''', (fName, lName, gender, image, description, 
-	email, password, bracePlacement, spaceOrTab, indentAmount, varConvention, commentStyle, maxLineLength)
+	email, password, bracePlacement, spaceOrTab, indentAmount, varConvention, commentStyle, maxLineLength))
+"""
 
-
-
-print("opened db")
+print("db created")
 
 conn.close()
 
-def functionName (arg1)
-	cursor = conn.execute('''SELECT id, bracePlacement, spaceOrTab, indentAmount, varConvention, commentStyle, maxLineLength
-	 FROM users WHERE email = ?;''', email)
+def functionName (arg1):
+	cursor = conn.execute('''SELECT id, bracePlacement, spaceOrTab, indentAmount, varConvention, commentStyle, maxLineLength 
+		FROM users WHERE email = ?;''', email)
 	for row in cursor:
 		id = row[0]
 		bracePlacement = row[1]
@@ -36,24 +44,18 @@ def functionName (arg1)
 		maxLineLength = row[6]	
 	cursor = conn.execute('''SELECT id, bracePlacement, spaceOrTab, indentAmount, varConvention, commentStyle, maxLineLength FROM users;''')
 	for row in cursor:
-		int score = 0;
-		if (row[1] == bracePlacement) {
-			score += 5;
-		}
-		if (row[2] == spaceOrTab) {
-			score += 5;
-		}
-		if (row[3] == indentAmount) {
-			score += 3;
-		}
-		if (row[4] == varConvention) {
-			score += 3;
-		}
-		if (row[5] == commentStyle) {
-			score += 1;
-		}
-		if (row[6] == maxLineLength) {
-			score += 1;
-		}
+		score = 0
+		if row[1] == bracePlacement:
+			score += 5
+		if row[2] == spaceOrTab:
+			score += 5
+		if row[3] == indentAmount:
+			score += 3
+		if row[4] == varConvention:
+			score += 3
+		if row[5] == commentStyle:
+			score += 1
+		if row[6] == maxLineLength:
+			score += 1
 	conn.execute('''INSERT INTO relationship (relationshipScore, idFirst, idSecond, statusFirst, statusSecond) values (?, ?, ?, ?, ?)''')
 	

2d716d19e5940494180e7f107a3f388a1308c6a0
Weilon <weilony@hotmail.com>
2016-10-01 15:44:31 +1000

    Server now uses jinja2 templating engine

--- server/server.py
+++ server/server.py
@@ -5,17 +5,23 @@ import random
 import string
 import os, os.path
 
+from jinja2 import Environment, FileSystemLoader
+env = Environment(loader=FileSystemLoader('public/html'))
+
 class Matchr(object):
     @cherrypy.expose
     def index(self):
-        f = open ('public/html/index.html', 'r') # open does not take cherrypy url
-        return f.read()
+        #f = open ('public/html/index.html', 'r') # open does not take cherrypy url
+        #return f.read()
+        tmpl = env.get_template('index.html')
+        return tmpl.render(name='John')
 
     @cherrypy.expose
     def generate(self, length=8):
         some_string = ''.join(random.sample(string.hexdigits, int(length)))
         cherrypy.session['mystring'] = some_string
-        return some_string
+        tmpl = env.get_template('generator.html')
+        return tmpl.render(string=some_string)
 
     @cherrypy.expose
     def display(self):

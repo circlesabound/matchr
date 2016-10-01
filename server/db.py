6da0a843a7b40ad86548b781b7b41c448e58d2a9
john <john@circlesabound.com>
2016-10-01 20:50:36 +1000

    Converted indentation from tabs to spaces

--- server/db.py
+++ server/db.py
@@ -3,27 +3,27 @@ import hashlib
 
 class db(object):
 
-	def __init__(self, db_path):
-		if not os.path.isfile(db_path):
-			raise FileNotFoundError
-		self.db_path = db_path
-		self.conn = None
+    def __init__(self, db_path):
+        if not os.path.isfile(db_path):
+            raise FileNotFoundError
+        self.db_path = db_path
+        self.conn = None
 
-	def connect(self):
-		self.conn = sqlite3.connect(db_path)
+    def connect(self):
+        self.conn = sqlite3.connect(db_path)
 
-	def close(self):
-		self.conn.close()
+    def close(self):
+        self.conn.close()
 
-	def check_credentials(self, email, password):
-		hashed = hashlib.sha256(password)
-		c = self.conn.cursor()
-		c.execute('SELECT Count(*), id FROM users WHERE email=? AND password=?', email, hashed.hexdigest())
-		(number_of_rows, user_id) = c.fetchone()
-		if number_of_rows is not None:
-			return (int)user_id
-		else:
-			return None
+    def check_credentials(self, email, password):
+        hashed = hashlib.sha256(password)
+        c = self.conn.cursor()
+        c.execute('SELECT Count(*), id FROM users WHERE email=? AND password=?', email, hashed.hexdigest())
+        (number_of_rows, user_id) = c.fetchone()
+        if number_of_rows is not None:
+            return (int)user_id
+        else:
+            return None
 
-	def get_next_match(self, id, ):
-		pass
+    def get_next_match(self, id, ):
+        pass
--- server/db_rebuild.py
+++ server/db_rebuild.py
@@ -5,17 +5,17 @@ import os.path
 import os
 
 if os.path.isfile('matchr.db'):
-	print("removing old db")
-	os.remove('matchr.db')
+    print("removing old db")
+    os.remove('matchr.db')
 
 conn = sqlite3.connect('matchr.db')
 
 conn.execute('''CREATE TABLE users (id INTEGER PRIMARY KEY, fName TEXT, lName TEXT, gender TEXT, 
-	image TEXT, description TEXT, email TEXT, password TEXT, bracePlacement INT, spaceOrTab INT, 
-	indentAmount INT, varConvention INT, commentStyle INT, maxLineLength INT);''')
+    image TEXT, description TEXT, email TEXT, password TEXT, bracePlacement INT, spaceOrTab INT, 
+    indentAmount INT, varConvention INT, commentStyle INT, maxLineLength INT);''')
 
 conn.execute('''CREATE TABLE relationship (relationshipScore INT, idFirst INT, 
-	idSecond INT, statusFirst INT, statusSecond INT);''')
+    idSecond INT, statusFirst INT, statusSecond INT);''')
 
 currentSession = (None,)
 conn.execute('''SELECT idSecond FROM relationship WHERE statusFirst = NULL AND idFirst = ? ORDER BY relationshipScore DESC;''', currentSession)
@@ -23,8 +23,8 @@ conn.execute('''SELECT idFirst FROM relationship WHERE statusSecond = NULL AND i
 
 """
 conn.execute('''INSERT INTO users (fName, lName, gender, image, description, email, password, bracePlacement, spaceOrTab, indentAmount, 
-	varConvention, commentStyle, maxLineLength) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);''', (fName, lName, gender, image, description, 
-	email, password, bracePlacement, spaceOrTab, indentAmount, varConvention, commentStyle, maxLineLength))
+    varConvention, commentStyle, maxLineLength) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);''', (fName, lName, gender, image, description, 
+    email, password, bracePlacement, spaceOrTab, indentAmount, varConvention, commentStyle, maxLineLength))
 """
 
 print("db created")
@@ -32,30 +32,30 @@ print("db created")
 conn.close()
 
 def functionName (arg1):
-	cursor = conn.execute('''SELECT id, bracePlacement, spaceOrTab, indentAmount, varConvention, commentStyle, maxLineLength 
-		FROM users WHERE email = ?;''', email)
-	for row in cursor:
-		id = row[0]
-		bracePlacement = row[1]
-		spaceOrTab = row[2]
-		indentAmount = row[3]
-		varConvention = row[4]
-		commentStyle = row[5]
-		maxLineLength = row[6]	
-	cursor = conn.execute('''SELECT id, bracePlacement, spaceOrTab, indentAmount, varConvention, commentStyle, maxLineLength FROM users;''')
-	for row in cursor:
-		score = 0
-		if row[1] == bracePlacement:
-			score += 5
-		if row[2] == spaceOrTab:
-			score += 5
-		if row[3] == indentAmount:
-			score += 3
-		if row[4] == varConvention:
-			score += 3
-		if row[5] == commentStyle:
-			score += 1
-		if row[6] == maxLineLength:
-			score += 1
-	conn.execute('''INSERT INTO relationship (relationshipScore, idFirst, idSecond, statusFirst, statusSecond) values (?, ?, ?, ?, ?)''')
-	
+    cursor = conn.execute('''SELECT id, bracePlacement, spaceOrTab, indentAmount, varConvention, commentStyle, maxLineLength 
+        FROM users WHERE email = ?;''', email)
+    for row in cursor:
+        id = row[0]
+        bracePlacement = row[1]
+        spaceOrTab = row[2]
+        indentAmount = row[3]
+        varConvention = row[4]
+        commentStyle = row[5]
+        maxLineLength = row[6]  
+    cursor = conn.execute('''SELECT id, bracePlacement, spaceOrTab, indentAmount, varConvention, commentStyle, maxLineLength FROM users;''')
+    for row in cursor:
+        score = 0
+        if row[1] == bracePlacement:
+            score += 5
+        if row[2] == spaceOrTab:
+            score += 5
+        if row[3] == indentAmount:
+            score += 3
+        if row[4] == varConvention:
+            score += 3
+        if row[5] == commentStyle:
+            score += 1
+        if row[6] == maxLineLength:
+            score += 1
+    conn.execute('''INSERT INTO relationship (relationshipScore, idFirst, idSecond, statusFirst, statusSecond) values (?, ?, ?, ?, ?)''')
+    

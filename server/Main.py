7238b16e45ced30b78f43b2f432eebd2ab69ab06
john <john@circlesabound.com>
2016-10-02 13:53:52 +1100

    dead matcher page

--- server/Main.py
+++ server/Main.py
@@ -8,16 +8,24 @@ class MainController(object):
     @cherrypy.expose
     @Auth.require()
     def matcher(self):
+        db = DB.DB('matchr.db')
+        db.connect()
+        match_id = db.get_next_match(cherrypy.session['user_details']['user_id'])
+        if match_id is None:
+            return env.get_template('dead_matcher.html').render()
         tmpl = env.get_template('matcher.html')
-        user_details = cherrypy.session['user_details']
         gender = ""
-        if user_details["gender"] is not None:
-        	gender = user_details["gender"].upper()
+        if match_details["gender"] is not None:
+            gender = match_details["gender"].upper()
         return tmpl.render(
-        	first_name=user_details['first_name'],
-        	last_name=user_details['last_name'],
-        	age="",
-        	gender=gender,
-        	image=user_details['image'],
-        	description=user_details['description']
-        )
+            first_name=match_details['first_name'],
+            last_name=match_details['last_name'],
+            age="",
+            gender=gender,
+            image=match_details['image'],
+            description=match_details['description'])
+
+    @cherrypy.expose
+    @Auth.require()
+    def reject(self):
+        pass
